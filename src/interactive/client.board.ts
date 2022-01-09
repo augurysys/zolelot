2300c39f7b60c323ae6f0c184fbbbcd4d499da01
Mark <msoifer@augury.com>
2022-01-09 16:37:17 +0200

    create interactive

---
+++ src/interactive/client.board.ts
@@ -0,0 +1,11 @@
+import { ICell } from "./board.types";
+
+export function StartGameAction(){}
+
+export function ClickOnCellAction(cell: ICell){}
+
+export function UpdateCellStatusAction(cell: ICell){}
+
+export function ReduceNumOfMoves(){ }
+
+export function ReduceNumOfLiveShipes(){}
---
+++ src/interactive/start_game.ts
@@ -0,0 +1,53 @@
+import { ECellStatus, ICell } from "./board.types";
+
+export interface IStartGame {
+    generateBoard(): void;
+    play(): void;
+    updateCellStatus(): void;
+}
+
+export class StartGame implements IStartGame {
+    play(): void {
+        throw new Error("Method not implemented.");
+    }
+
+
+    generateBoard(): void {
+        //Gil and Keren implementaion
+        throw new Error("Method not implemented.");
+    }
+
+    onCellClick(cell: ICell) {
+        if (validateMove()) {
+            this.updateCellStatus(cell);
+        }
+        else {
+            throw console.log('please choose another cell!!!!')
+        }
+    }
+
+    updateCellStatus(cell: ICell): void {
+        switch (cell.status) {
+            case ECellStatus.Empty:
+                UpdateCellStatusAction(ECellStatus.Active)
+                break;
+            case ECellStatus.Active:
+                UpdateCellStatusAction(ECellStatus.Killed)
+                break;
+            default:
+                break;
+        }
+    }
+
+    isGameCompleted(): boolean {
+       return  this.validateNumOfMoves() || this.validateNumOfLiveShips()
+    }
+
+    validateNumOfMoves(): boolean {
+        return store.MaxNumOfMoves > 0;
+    }
+
+    validateNumOfLiveShips(): boolean {
+        return store.NumOfShipCells > 0;
+    }
+}
